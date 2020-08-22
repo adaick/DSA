6f2c979007c65d32599ace8b9ca471bad094d7d6
Adarsh Mallick <38970527+adaick@users.noreply.github.com>
2020-08-22 15:14:23 +0530

    removed one error today

--- Test/stack.py
+++ Test/stack.py
@@ -1,24 +1,24 @@
 class MaxStack:
     def __init__(self):
-        self.stack = []
-        self.stack = []
+        self.stackmain = []
+        self.stackmax = []
 
     def push(self, data):
-        self.stack.append(data)
+        self.stackmain.append(data)
 
-        if len(self.main_stack) == 1:
-            self.stack.append(data)
-        elif data > self.max_stack[-1]:
-            self.stack.append(data)
+        if len(self.stackmain) == 1:
+            self.stackmax.append(data)
+        elif data > self.stackmax[-1]:
+            self.stackmax.append(data)
         else:
-            self.stack.append(self.max_stack[-1])
+            self.stackmax.append(self.stackmax[-1])
 
     def pop(self):
-        self.stack.pop()
-        return self.stack.pop()
+        self.stackmax.pop()
+        return self.stackmain.pop()
 
     def maximum(self):
-        return self.stack[-1]
+        return self.stackmax[-1]
 
 stack = MaxStack()
 n = int(input("Enter number of commands: "))
@@ -26,7 +26,7 @@ for i in range(n):
     inputt = input().split()
     if inputt[0] == 'push':
         stack.push(inputt[1])
-    elif inputtinputt[0] == 'pop':
+    elif inputt[0] == 'pop':
         stack.pop()
     elif inputt[0] == "max":
         print(stack.maximum())
